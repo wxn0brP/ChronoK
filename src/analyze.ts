c79b19ff2f079bc5e0f7d6e220b8f27e7671e402
wxn0brP <toja.filiper@gmail.com>
2025-09-27 19:40:09 +0200

    feat: update

--- src/analyze.ts
+++ src/analyze.ts
@@ -12,17 +12,34 @@ function analyze(events: Evt[]) {
         console.log("Last event not ended, assuming now");
     }
 
-    const totalTime = events.reduce((sum, e) => sum + (e.end - e.start), 0);
+    const mergedEvents: Evt[] = [];
+    if (events.length > 0) {
+        mergedEvents.push({ ...events[0] });
+        for (let i = 1; i < events.length; i++) {
+            const prevEvent = mergedEvents[mergedEvents.length - 1];
+            const currentEvent = events[i];
+            const gap = currentEvent.start - prevEvent.end;
 
-    const avgTime = totalTime / events.length;
+            if (gap < 5000) {
+                prevEvent.end = currentEvent.end;
+            } else {
+                mergedEvents.push({ ...currentEvent });
+            }
+        }
+    }
+
+    const totalTime = mergedEvents.reduce((sum, e) => sum + (e.end - e.start), 0);
+
+    const avgTime = totalTime / mergedEvents.length;
 
     const gaps: number[] = [];
-    for (let i = 1; i < events.length; i++) {
-        gaps.push(events[i].start - events[i - 1].end);
+    for (let i = 1; i < mergedEvents.length; i++) {
+        gaps.push(mergedEvents[i].start - mergedEvents[i - 1].end);
     }
 
     console.log("Event analysis:");
-    console.log("Number of events:\t", events.length);
+    console.log("Merged events:\t\t", mergedEvents.length);
+    console.log("Events Count:\t\t", events.length);
     console.log("Total time:\t\t", formatTime(totalTime));
     console.log("Average event time:\t", formatTime(avgTime));
     console.log("Gaps between events:\t", gaps.map(formatTime).filter(Boolean).join(", "));
